0e68e615be9ac24fed19324b87a8a0af29d515a5
Vishal Tayade <tayadevishal00@gmail.com>
2024-10-02 21:01:12 +0530

    sync time and clock

--- JS/script.js
+++ JS/script.js
@@ -6,13 +6,25 @@ hourHand.style.transformOrigin = '0';
 minuteHand.style.transformOrigin = '0';
 secondHand.style.transformOrigin = '46px';
 
-
-// let [hr, min, sec] = [0, 0, 0];
-
 setInterval(()=>{
     let date = new Date();
-    const [hr, min, sec] = [date.getHours(), date.getMinutes(), date.getSeconds()];
-    hourHand.style.transform = `rotate(${hr}deg)`;
-    minuteHand.style.transform = `rotate(${min}deg)`;
-    secondHand.style.transform = `rotate(${sec}deg)`;
-},0);
+    let time = date.toLocaleTimeString();
+    time = time.split(':');
+    
+    // let [hour, minute, second] = [date.getHours(), date.getMinutes(), date.getSeconds()];
+    let [hour, minute, second] = [time[0], time[1], time[2].slice(0,2)];
+    // let [hour, minute, second] = [12,59,59];
+    // console.log(hour, minute, second);
+    let min = minute * 6;
+    let hr = (hour * 30) + (min/12);
+    let sec = second * 6;
+
+    
+
+    hourHand.style.transform = `rotate(${hr-90}deg)`;
+
+    minuteHand.style.transform = `rotate(${min-90}deg)`;
+
+    secondHand.style.transform = `rotate(${sec-90}deg)`;
+    
+},1000);
